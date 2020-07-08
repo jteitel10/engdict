f7252d636a85127a48238587f821659d0230e578
jteitel10 <jteitel10@gmail.com>
2020-07-08 11:23:00 -0400

    Implemented case sensitivty as well as an error message if word is not in data file

--- script.py
+++ script.py
@@ -4,9 +4,12 @@ data = json.load(open('data.json'))
 
 
 def fooTranslate(word):
-    return data[word]
+    if word in data:
+        return data[word]
+    else:
+        return "Word not defined or does not exist. Please try again."
 
 
 input_word = input('Enter word: ')
 
-print(fooTranslate(input_word))
+print(fooTranslate(input_word.lower()))
