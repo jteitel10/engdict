67d92dd8158965e30feca9bb5855fe8408d68542
jteitel10 <jteitel10@gmail.com>
2020-07-08 11:51:45 -0400

    Implemented functionalities to find close match words in case of typo.  User is prompted to confirm the selected word and is returned the definition without ending the program.

--- script.py
+++ script.py
@@ -1,15 +1,24 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open('data.json'))
 
 
 def fooTranslate(word):
+    word = word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input("Did you mean {} instead?  Enter y or n. ".format(
+            get_close_matches(word, data.keys())[0]))
+        if yn == 'y':
+            return data[get_close_matches(word, data.keys())[0]]
+        else:
+            return 'Word not defined or does not exist.  Please try again.'
     else:
         return "Word not defined or does not exist. Please try again."
 
 
 input_word = input('Enter word: ')
 
-print(fooTranslate(input_word.lower()))
+print(fooTranslate(input_word))
