16d1a862b8ec7fa2c47641c18d78b9911874b057
jteitel10 <jteitel10@gmail.com>
2020-07-08 12:52:22 -0400

    Updated the fooTranslate to allow for acronyms to be searched.

--- script.py
+++ script.py
@@ -8,17 +8,29 @@ def fooTranslate(word):
     word = word.lower()
     if word in data:
         return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
     elif len(get_close_matches(word, data.keys())) > 0:
         yn = input("Did you mean {} instead?  Enter y or n. ".format(
             get_close_matches(word, data.keys())[0]))
-        if yn == 'y':
+        if yn.lower() == 'y':
             return data[get_close_matches(word, data.keys())[0]]
-        else:
+        elif yn.lower() == 'n':
             return 'Word not defined or does not exist.  Please try again.'
+        else:
+            return 'Entry not understood.  Please try again.'
     else:
         return "Word not defined or does not exist. Please try again."
 
 
 input_word = input('Enter word: ')
 
-print(fooTranslate(input_word))
+output = fooTranslate(input_word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
